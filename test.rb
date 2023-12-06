0e5b2ef974ec57c02c17f47c7d30f23756f2b146
Markus <mareme100@gmail.com>
2023-12-06 15:06:40 -0600

    Added ActiveAdmin

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'faker'
+
+# Create categories
+4.times do
+  Category.create(name: Faker::Commerce.department)
+end
+
+# Create products
+100.times do
+  product = Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price,
+    category_id: Category.pluck(:id).sample
+  )
+end
