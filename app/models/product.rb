99c828af4c98ccb0c8ab1a1d8eca05d7d3f9c328
Markus <mareme100@gmail.com>
2023-12-07 13:54:08 -0600

    View list/individual products and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["category_id", "created_at", "description", "id", "name", "price", "updated_at"]
+  end
 end
