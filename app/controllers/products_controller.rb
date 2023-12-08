8808cbf564e928f5a6e98b011a17aa28ef2bbad0
Markus <mareme100@gmail.com>
2023-12-08 13:02:23 -0600

    Image funtionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,28 @@
 class ProductsController < ApplicationController
   def index
-    def index
-      @selected_filter = params[:filter] || 'all'
-      @selected_category = params[:category]
-      @search = params[:search]
+    @selected_filter = params[:filter] || 'all'
+    @selected_category = params[:category]
+    @search = params[:search]
 
-      @filtered_products = Product.all
+    @filtered_products = Product.all
 
-      @filtered_products = @filtered_products.where(category: @selected_category) if @selected_category.present?
+    # Apply category filter
+    @filtered_products = @filtered_products.where(category: @selected_category) if @selected_category.present?
 
-      @filtered_products = @filtered_products.ransack(name_or_description_cont: @search).result if @search.present?
+    # Apply keyword search
+    @filtered_products = @filtered_products.ransack(name_or_description_cont: @search).result if @search.present?
 
-      case @selected_filter
-      when 'new'
-        @filtered_products = @filtered_products.where('created_at >= ?', 3.days.ago).order(created_at: :desc)
-      when 'recently_updated'
-        @filtered_products = @filtered_products.where('updated_at >= ?', 1.days.ago).order(updated_at: :desc)
-      end
-
-      @filtered_products = @filtered_products.page(params[:page]).per(15)
+    # Apply additional filters based on @selected_filter
+    case @selected_filter
+    when 'new'
+      @filtered_products = @filtered_products.where('created_at >= ?', 3.days.ago).order(created_at: :desc)
+    when 'recently_updated'
+      @filtered_products = @filtered_products.where('updated_at >= ?', 1.days.ago).order(updated_at: :desc)
     end
 
+    @filtered_products = @filtered_products.page(params[:page]).per(15) if @filtered_products
+
+    # rest of your action...
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_one_attached :image
 
   def self.search(category, keyword)
     products = all
@@ -11,6 +12,20 @@ class Product < ApplicationRecord
     end
 
     products
+  end
+
+  attr_accessor :remove_image
+  before_save :remove_image_if_requested
+  private
+
+  def remove_image_if_requested
+    self.image = nil if remove_image == '1' || remove_image == true
+  end
+
+  attr_accessor :image_attachment_id_filter
+
+  def self.ransackable_attributes(auth_object = nil)
+    super + ['image_attachment_id_filter']
   end
 
   def self.ransackable_attributes(auth_object = nil)
