a9c113971017153c287a5850b68b48319a2b9845
Markus <mareme100@gmail.com>
2023-12-09 16:06:48 -0600

    Removing useless code

--- app/models/order.rb
+++
@@ -1,8 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :customer
-  has_many :order_items, dependent: :destroy
-
-  def self.ransackable_attributes(auth_object = nil)
-    ["address", "created_at", "customer_id", "id", "province", "total", "updated_at"]
-  end
-end
